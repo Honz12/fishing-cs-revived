e26b3e33809c1c32f75f0b432f17db803a5aede5
mistrmatej <matej.albert11@gmail.com>
2026-08-01 13:30:32 +0200

    Pridany prvni advancementy

--- src/Advancements.cs
+++ src/Advancements.cs
@@ -3,7 +3,10 @@ class Advancement
     public required string Name;
     public required string Description;
     public required string IconName;
+    public required bool IsUnlocked;
 }
+
+
 
 static class AdvancementProcessor
 {
@@ -13,8 +16,51 @@ static class AdvancementProcessor
     /// </summary>
     /// <param name="playerData"></param>
     /// <returns></returns>
+    public static int AdvancementCount = 0;
     public static Advancement? CheckForNewAdvancements(PlayerData playerData)
     {
         return null;
     }
+}
+
+class AdvancementData
+{
+    public static Advancement[] advancements =
+    {
+        new Advancement()
+        {
+            Name = "1. Úlovek!",
+            Description = "Chyť svou první rybu.",
+            IconName = "prvniUlovek.txt",
+            IsUnlocked = false
+        },
+        new Advancement()
+        {
+            Name = "Konečně pořádný prut!",
+            Description = "Upgraduj poprvé svůj prut.",
+            IconName = "poradnyPrut.txt",
+            IsUnlocked = false
+        },
+        new Advancement()
+        {
+            Name = "Všude dobře, doma nejlíp!",
+            Description = "Kup si svůj první dům.",
+            IconName = "domaNejlip.txt",
+            IsUnlocked = false
+        },
+        new Advancement()
+        {
+            Name = "Co to je?!",
+            Description = "Chyť Krakena.",
+            IconName = "chytKrakena.txt",
+            IsUnlocked = false
+        },
+        new Advancement()
+        {
+            Name = "Tátův telefonát.",
+            Description = "Táta ti zavolal.",
+            IconName = "tatuvTelefonat.txt",
+            IsUnlocked = false
+        }
+    };
 }
