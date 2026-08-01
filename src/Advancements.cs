c829b79c90318ca86b258faffe45429db6c8c18a
mistrmatej <matej.albert11@gmail.com>
2026-08-01 14:03:49 +0200

    Predelavka

--- src/Advancements.cs
+++ src/Advancements.cs
@@ -3,13 +3,15 @@ class Advancement
     public required string Name;
     public required string Description;
     public required string IconName;
-    public required bool IsUnlocked;
+    public required string Id;
 }
 
 
 
 static class AdvancementProcessor
 {
+    static List<string> unlockedAdvancementIds = new();
+
     /// <summary>
     /// Checks if the player has unlocked any new advancements based on their current data.
     /// If not returns null, otherwise returns the Advancement object that was unlocked.
@@ -19,48 +21,69 @@ static class AdvancementProcessor
     public static int AdvancementCount = 0;
     public static Advancement? CheckForNewAdvancements(PlayerData playerData)
     {
-        return null;
-    }
-}
-
-class AdvancementData
-{
-    public static Advancement[] advancements =
-    {
-        new Advancement()
+        if (playerData.Inventory.Count > 0 && !unlockedAdvancementIds.Contains("prvniUlovek"))
         {
-            Name = "1. Úlovek!",
-            Description = "Chyť svou první rybu.",
-            IconName = "prvniUlovek.txt",
-            IsUnlocked = false
-        },
-        new Advancement()
-        {
-            Name = "Konečně pořádný prut!",
-            Description = "Upgraduj poprvé svůj prut.",
-            IconName = "poradnyPrut.txt",
-            IsUnlocked = false
-        },
-        new Advancement()
-        {
-            Name = "Všude dobře, doma nejlíp!",
-            Description = "Kup si svůj první dům.",
-            IconName = "domaNejlip.txt",
-            IsUnlocked = false
-        },
-        new Advancement()
-        {
-            Name = "Co to je?!",
-            Description = "Chyť Krakena.",
-            IconName = "chytKrakena.txt",
-            IsUnlocked = false
-        },
-        new Advancement()
-        {
-            Name = "Tátův telefonát.",
-            Description = "Táta ti zavolal.",
-            IconName = "tatuvTelefonat.txt",
-            IsUnlocked = false
+            unlockedAdvancementIds.Add("prvniUlovek");
+            return new Advancement()
+            {
+                Name = "1. Úlovek!",
+                Description = "Chyť svou první rybu.",
+                IconName = "prvniUlovek.txt",
+                Id = "prvniUlovek"
+            };
         }
-    };
+
+        else if (Program.data.RodLevel = 1 && !unlockedAdvancementIds.Contains("poradnyPrut"))
+        {
+            unlockedAdvancementIds.Add("poradnyPrut");
+            return new Advancement()
+            {
+                Name = "Konečně pořádný prut!",
+                Description = "Upgraduj poprvé svůj prut.",
+                IconName = "poradnyPrut.txt",
+                Id = "poradnyPrut"
+            };
+        }
+        
+        else if (Program.data.HouseLevel = 1 && !unlockedAdvancementIds.Contains("domaNejlip"))
+        {
+            unlockedAdvancementIds.Add("domaNejlip");
+            return new Advancement()
+            {
+                Name = "Všude dobře, doma nejlíp!",
+                Description = "Kup si svůj první dům.",
+                IconName = "domaNejlip.txt",
+                Id = "domaNejlip"
+            };
+        }
+        
+        else if (Program.data.HouseLevel = 5 && !unlockedAdvancementIds.Contains("chytKrakena")) //upravit pro case ze mam krakena
+        {
+            unlockedAdvancementIds.Add("chytKrakena");
+            return new Advancement()
+            {
+                Name = "Co to je?!",
+                Description = "Chyť Krakena.",
+                IconName = "chytKrakena.txt",
+                Id = "chytKrakena"
+            };
+        }
+        
+        else if (Program.data.HouseLevel >= 4 && !unlockedAdvancementIds.Contains("tatuvTelefonat"))
+        {
+            unlockedAdvancementIds.Add("tatuvTelefonat");
+            return new Advancement()
+            {
+                Name = "Tátův telefonát.",
+                Description = "Táta ti zavolal.",
+                IconName = "tatuvTelefonat.txt",
+                Id = "tatuvTelefonat.txt"
+            };
+        }
+        
+        else
+        {
+            return null; 
+        } 
+    }
 }
