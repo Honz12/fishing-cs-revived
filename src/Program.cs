473b1ec2d255b6f6ac63d482bdf530d56a7f20b1
Honz12 <jparik3@gmail.com>
2026-08-01 13:59:53 +0200

    Finish engine for advancements

--- src/Program.cs
+++ src/Program.cs
@@ -35,7 +35,7 @@ class Program
     public static PlayerData data = new();
     private static int catchingPos = 0;
     private static uint gameTicks = 0;
-    private static uint successfullyCatchingTicks = 0;
+    private static int successfullyCatchingTicks = 0;
     private static int catchingCenterSize = 0;
     private static Fish? catchingFish = null;
     private static uint requiredCatchingTicks = 0;
@@ -360,6 +360,18 @@ sebastianjecny-green
         Console.ReadKey(true);
     }
 
+    private static void CheckForNewAdvancements()
+    {
+        Advancement? newAdvancement = AdvancementProcessor.CheckForNewAdvancements(data);
+        while (newAdvancement != null)
+        {
+            data.Advancements.Add(newAdvancement);
+            Console.WriteLine($"Nový Advancment odemčen: {newAdvancement.Name} ({newAdvancement.Description})");
+            Console.ReadKey(true);
+            newAdvancement = AdvancementProcessor.CheckForNewAdvancements(data);
+        }
+    }
+
     /// <summary>
     /// The main function,
     /// what would you expect?
@@ -442,6 +454,7 @@ sebastianjecny-green
                             case ConsoleKey.Spacebar:
                             case ConsoleKey.Enter:
                                 Shop.EnterOption(data);
+                                CheckForNewAdvancements();
                                 break;
                             case ConsoleKey.Escape:
                                 data.GameState = GameState.MainMenu;
@@ -561,7 +574,7 @@ sebastianjecny-green
                         if (catchingOffset > 20)
                             catchingVel = -1;
 
-                        if (successfullyCatchingTicks > 0xFFFF) // We can surely say the position overflowed.
+                        if (successfullyCatchingTicks < 0)
                         {
                             Console.WriteLine("Ryba uplavala!");
                             DisplayImage((catchingFish ?? new Fish()).Image, (catchingFish ?? new Fish()).GetFormatedData());
@@ -585,6 +598,7 @@ sebastianjecny-green
                             {
                                 data.Inventory.Add(catchingFish ?? new Fish());
                             }
+                            CheckForNewAdvancements();
                             data.GameState = GameState.MainMenu;
                             currentlyCatching = false;
                         }
@@ -613,6 +627,7 @@ sebastianjecny-green
                                 break;
                             case ConsoleKey.S:
                                 InventoryUi.SellOption(data);
+                                CheckForNewAdvancements();
                                 break;
                             case ConsoleKey.Escape:
                                 data.GameState = GameState.MainMenu;
